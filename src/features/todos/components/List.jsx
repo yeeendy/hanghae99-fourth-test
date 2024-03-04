d9a3af48ead58cce46d91882d08e7d25c4e7603c
LEE YEJIN <dldulwls@gmail.com>
2024-03-04 11:26:01 +0900

    [FIX]: Done 부분 상세보기 기능 수정

--- src/features/todos/components/List.jsx
+++ src/features/todos/components/List.jsx
@@ -58,7 +58,7 @@ const List = () => {
           if (todo.isDone) {
             return (
               <StTodoContainer key={todo.id}>
-                <StLink to={`/${index}`} key={todo.id}>
+                <StLink to={`/${todo.id}`} key={todo.id}>
                   <div>상세보기</div>
                 </StLink>
                 <div>
